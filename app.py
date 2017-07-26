91f0056f61c048fd28bf4c9725a5725edb908b2c
sefa saglam <sefaoguzsaglam@gmail.com>
2017-07-26 14:21:27 +0200

    wr

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 from flask import Flask,render_template,request,redirect
-app = Flask(__name__)
+app_lulu = Flask(__name__)
 
-app.vars={}
+app_lulu.vars={}
 
-app.questions={}
-app.questions['How many eyes do you have?']=('1','2','3')
-app.questions['Which fruit do you like best?']=('banana','mango','pineapple')
-app.questions['Do you like cupcakes?']=('yes','no','maybe')
+app_lulu.questions={}
+app_lulu.questions['How many eyes do you have?']=('1','2','3')
+app_lulu.questions['Which fruit do you like best?']=('banana','mango','pineapple')
+app_lulu.questions['Do you like cupcakes?']=('yes','no','maybe')
 
-app.nquestions=len(app.questions)
+app_lulu.nquestions=len(app_lulu.questions)
 # should be 3
 
-@app.route('/index_lulu',methods=['GET','POST'])
+@app_lulu.route('/index_lulu',methods=['GET','POST'])
 def index_lulu():
-    nquestions=app.nquestions
+    nquestions=app_lulu.nquestions
     if request.method == 'GET':
         return render_template('userinfo_lulu.html',num=nquestions)
     else:
         # request was a POST
-        app.vars['name'] = request.form['name_lulu']
-        app.vars['age'] = request.form['age_lulu']
+        app_lulu.vars['name'] = request.form['name_lulu']
+        app_lulu.vars['age'] = request.form['age_lulu']
 
-        f = open('%s_%s.txt'%(app.vars['name'],app.vars['age']),'w')
-        f.write('Name: %s\n'%(app.vars['name']))
-        f.write('Age: %s\n\n'%(app.vars['age']))
+        f = open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'w')
+        f.write('Name: %s\n'%(app_lulu.vars['name']))
+        f.write('Age: %s\n\n'%(app_lulu.vars['age']))
         f.close()
 
         return redirect('/main_lulu')
 
-@app.route('/main_lulu')
+@app_lulu.route('/main_lulu')
 def main_lulu2():
-    if len(app.questions)==0 : return render_template('end_lulu.html')
+    if len(app_lulu.questions)==0 : return render_template('end_lulu.html')
     return redirect('/next_lulu')
 
 #####################################
@@ -39,33 +39,33 @@ def main_lulu2():
 ## The attribute that contains GET and POST is: request.method
 #####################################
 
-@app.route('/next_lulu',methods=['GET'])
+@app_lulu.route('/next_lulu',methods=['GET'])
 def next_lulu(): #remember the function name does not need to match the URL
     # for clarity (temp variables)
-    n = app.nquestions - len(app.questions) + 1
-    q = list(app.questions.keys())[0] #python indexes at 0
-    a1, a2, a3 = list(app.questions.values())[0] #this will return the answers corresponding to q
+    n = app_lulu.nquestions - len(app_lulu.questions) + 1
+    q = list(app_lulu.questions.keys())[0] #python indexes at 0
+    a1, a2, a3 = list(app_lulu.questions.values())[0] #this will return the answers corresponding to q
 
     # save the current question key
-    app.currentq = q
+    app_lulu.currentq = q
 
     return render_template('layout_lulu.html',num=n,question=q,ans1=a1,ans2=a2,ans3=a3)
 
-@app.route('/next_lulu',methods=['POST'])
+@app_lulu.route('/next_lulu',methods=['POST'])
 def next_lulu2():  #can't have two functions with the same name
     # Here, we will collect data from the user.
     # Then, we return to the main function, so it can tell us whether to
     # display another question page, or to show the end page.
 
-    f = open('%s_%s.txt'%(app.vars['name'],app.vars['age']),'a') #a is for append
-    f.write('%s\n'%(app.currentq))
+    f = open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'a') #a is for append
+    f.write('%s\n'%(app_lulu.currentq))
     f.write('%s\n\n'%(request.form['answer_from_layout_lulu'])) #this was the 'name' on layout.html!
     f.close()
 
     # Remove question from dictionary
-    del app.questions[app.currentq]
+    del app_lulu.questions[app_lulu.currentq]
 )
     return redirect('/main_lulu')
 
 if __name__ == "__main__":
-    app.run(port=33507
+    app_lulu.run(port=33507
